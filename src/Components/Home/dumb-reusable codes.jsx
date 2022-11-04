f5d3fb6a010009cf68cfe4a6172ab30f5796364b
muzammil <muzofficial90@gmail.com>
2022-11-04 12:44:21 +0000

    Added Sorting,Design Changes

--- src/Components/Home/dumb-reusable codes.jsx
+++ src/Components/Home/dumb-reusable codes.jsx
@@ -49,3 +49,116 @@ const isValidImage = async (url) => {
         return false;
     }
 };
+
+import { useEffect, useState } from 'react';
+// material
+import { Menu, Button, MenuItem, Typography } from '@mui/material';
+// component
+// import Iconify from '../../../components/Iconify';
+import Iconify from 'src/components/Iconify';
+import * as _ from "lodash";
+import { useContext } from 'react';
+import { SortResult } from './ResultV2';
+
+// ----------------------------------------------------------------------
+
+
+
+export default function ResultSort() {
+  const [open, setOpen] = useState(null);
+  let localProducts
+  const { products, setProducts } = useContext(SortResult)
+
+  const [label, setLabel] = useState('')
+  
+  const handleOpen = (event) => {
+    setOpen(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setOpen(null);
+  };
+
+  const SORT_BY_OPTIONS = [
+    { value: 'name', state: '-', label: 'Featured' },
+    { value: 'postDate', state: 'date', label: 'Newest' },
+    { value: 'price', state: 'priceHigh', label: 'Price: Low-High' },
+    { value: 'price', state: '-', label: 'Price: High-Low' }
+  ];
+
+  const handleSort = (props, state) => {
+    console.log(props, state);
+    let sorted
+    if (products == null) {
+      console.log('storedArray NULL Found FIX THIS');
+    } else {
+      localProducts = products
+      if (state === 'date') {
+        console.log('sort by date');
+        // sorted = _.sortBy(localProducts, 'date').reverse()
+        sorted = _.sortBy(localProducts, 'postDate').reverse()
+        // sorted = localProducts.sort((a, b) =>
+        //   a.postDate.split('/').reverse().join().localeCompare(b.postDate.split('/').reverse().join()))
+      } else if (props === 'price' && state === 'priceHigh') {
+        // sorted = localProducts.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+        sorted = _.sortBy(localProducts, 'price')
+      } else if (props === 'price') {
+        // sorted = localProducts.sort((a, b) => parseFloat(b.price) - parseFloat(a.price));
+        sorted = _.sortBy(localProducts, 'price').reverse()
+      } else {
+        sorted = _.sortBy(localProducts, 'name')
+        // sorted = _.sortBy(localProducts, ['name','price'])
+      }
+
+      // sessionStorage.setItem("localProducts", JSON.stringify(sorted));
+      // setReload(true)
+      console.log('SORTED DATA',sorted);
+      setProducts(sorted)
+      setOpen(null)
+      // setReload(true)
+      // state= null
+      // props = null
+    }
+
+  }
+  // useEffect(() => {
+
+  // }, [handleSort])
+  return (
+    <>
+      <Button
+        color="inherit"
+        disableRipple
+        onClick={handleOpen}
+        endIcon={<Iconify icon={open ? 'eva:chevron-up-fill' : 'eva:chevron-down-fill'} />}
+      >
+        Sort By:&nbsp;
+        <Typography component="span" variant="subtitle2" sx={{ color: 'text.secondary' }}>
+          {label}
+        </Typography>
+      </Button>
+      <Menu
+        keepMounted
+        anchorEl={open}
+        open={Boolean(open)}
+        onClose={handleClose}
+        // onClick={() => { setReload(false) }}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      >
+        {SORT_BY_OPTIONS.map((option) => (
+          <MenuItem
+            key={option.label}
+            // selected={!option.value===''}
+            // onChange={() => }
+            value={option.value}
+            onClick={() => { setLabel(option.label); handleSort(option.value, option.state) }}
+            sx={{ typography: 'body2' }}
+          >
+            {option.label}
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
+  );
+}
--- src/Pages/WatchLog/WatchLog.jsx
+++ src/Pages/WatchLog/WatchLog.jsx
@@ -2,34 +2,106 @@ import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../../Components/Header/Header';
 import { DataContext } from '../../Contexts/DataContext';
+import { Menu, Button, MenuItem, Typography, Popover, Avatar, Badge } from '@mui/material';
 import './WatchLog.scss';
+import Iconify from '../../Hooks/Iconify';
+import * as _ from 'lodash';
 
 function WatchLog() {
     const route = useNavigate();
     const { movieLog } = useContext(DataContext);
-    // let movieLog = ['hf','f']
+    const [watchLog, setWatchLog] = useState([]);
+    const [view, setView] = useState(true);
+    const [openSort, setOpenSort] = useState(null);
+    const [label, setLabel] = useState('');
 
-    console.log('logFF', movieLog);
+    useEffect(() => {
+        console.log('logFF', movieLog);
+        setWatchLog(movieLog);
+        if (movieLog.length <= 0) {
+            setView(false);
+        }
+    }, []);
+
+    const SORT_BY_OPTIONS = [
+        { value: 'name', state: '-', label: 'Featured' },
+        { value: 'id', state: 'date', label: 'Recently' },
+        { value: 'watchCount', state: 'priceHigh', label: 'Most Watched' },
+        { value: 'createdAt', state: 'priceHigh', label: 'Created At' },
+        { value: 'year', state: 'priceHigh', label: 'Release' },
+        { value: 'watchedDate', state: 'priceHigh', label: 'Yearly' },
+
+    ];
+    const handleSort = (e, value, action) => {
+        e.preventDefault();
+        console.log(value);
+        let data = _.sortBy(watchLog, value).reverse();
+        setWatchLog(data);
+    };
+
+    const handleSort2 = (e, value, action) => {
+        e.preventDefault();
+        console.log(value);
+        setWatchLog(_.sortBy(watchLog, value).reverse());
+    };
+
     return (
         <div >
             <Header />
             <div className="container-fluid watchLog">
                 <div className="row center Title">
                     <h2 className='mainHead'>My WatchLog</h2>
-                    {movieLog?.length === 0 && <>
+                    {watchLog?.length === 0 && <>
                         <p>You have'nt added anything in your movieLog! <br />Please Add Titles to View Here</p>
                         <button onClick={e => route('/addItem')} className="navigateBtn">Add Title</button> </>}
                     <p className='error'></p>
 
                 </div>
+                {view && <div className="sortWrapper">
+                    <div className="searcher">
+                        <input type="text" placeholder='Search From Titles...' />
+                        <button>Search</button>
+                    </div>
+                    <Button
+                        color="inherit"
+                        disableRipple
+                        onClick={e => setOpenSort(e.currentTarget)}
+                        endIcon={<Iconify icon={openSort ? 'bi:sort-up-alt' : 'bi:sort-down'} />}
+                    // startIcon={<Iconify icon='bxs:sort-alt' />}
+                    >Sort By:&nbsp;<b>{label}</b>
+                    </Button>
+                    <Popover
+                        anchorEl={openSort}
+                        open={Boolean(openSort)}
+                        onClose={e => setOpenSort(null)}
+                        // onClick={() => { setReload(false) }}
+                        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+                    > <div className="sortOption">
+                            {SORT_BY_OPTIONS.map((option) => (
+                                <button className='selectBtn' key={option.label} value={option.value}
+                                    onClick={(e) => {
+                                        setLabel(option.label); setOpenSort(null);
+                                        handleSort2(e, option.value, option.state)
+                                    }} >
+                                    {option.label}
+                                </button>
+                            ))}
+                        </div>
+                    </Popover>
+                </div>}
                 <div>
                     <div className="logContainer">
-                        {movieLog?.map((movie) => (
+
+                        {watchLog?.map((movie) => (
                             <div className="itemBg lozad" key={movie.id} style={{ backgroundImage: `url(${movie?.url})` }}>
-                                <h4 className='text'>{movie?.name}</h4>
-                                <h6 className='text'>Released: {movie?.year}</h6>
-                                <h5 className='text'>{movie?.watchedDate}</h5>
-                                <h6 className='text'>{movie?.watchedTime}</h6>
+                                <div className='text'>
+                                    <h4>{movie?.name}</h4>
+                                    <h6>Released: {movie?.year}</h6>
+                                    <h5>{movie?.watchedDate}</h5>
+                                    <h6>{movie?.watchedTime}</h6>
+                                    {!movie?.watchCount === 1 && <Badge badgeContent={movie?.watchCount} color="success" />}
+                                </div>
                                 {/* <p>Added On: {movie?.createdAt}</p> */}
                             </div>
                         ))}
