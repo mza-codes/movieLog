8561cfeb949918fbd2de22a01c5e385661d1711b
mza-codes <muzofficial90@gmail.com>
2022-12-16 17:24:12 +0000

    Added Changelog,Fixed Hover

---
+++ CHANGELOG.md
@@ -0,0 +1,18 @@
+## CHANGELOG
+
+
+#### Fixed Unnecessary hover (16/12/2022)
+#### Changed Homescreen
+#### Implementation With Zustand *(In Progress..)*
+#### 
+---
+## OLD
+#### --
+#### Added Navbar & Colors
+#### Added Bootstrap,Saas,MUI,Iconify etc.
+#### Setting Up Boilerplate from Scratch
+
+
+### mza-codes
+
+
--- src/Store/useDataStore.js
+++ src/Store/useDataStore.js
@@ -1,10 +1,11 @@
 import create from 'zustand';
-import { doc, getDoc } from "firebase/firestore";
+import { arrayUnion, doc, getDoc, updateDoc } from "firebase/firestore";
 import { db } from "../firebaseConfig/firebase";
 
 const initialState = {
     movieLog: [],
-    err: { active: false }
+    err: { active: false },
+    user: {},
 };
 
 const useDataStore = create((set, get) => ({
@@ -17,6 +18,7 @@ const useDataStore = create((set, get) => ({
             const data = res.data();
             set((state) => ({
                 ...state,
+                user: user,
                 movieLog: data?.watchData ?? []
             }));
             return true;
@@ -29,12 +31,36 @@ const useDataStore = create((set, get) => ({
             return false;
         };
     },
+    addToFirestore: async (data) => {
+        if (!data) return false;
+        const user = get().user;
+
+        await updateDoc(doc(db, 'webusers', user.uid), {
+            watchData: arrayUnion(data)
+        }).then((resp) => {
+            console.log('addedData', resp);
+            return true;
+        }).catch(e => {
+            console.log('Error Adding Data >> PromiseErr: ', e);
+            return false;
+        });
+    },
+    removeAnUpdate: async () => {
+        // 
+    },
     addOne: (item) => {
         if (!item) return false;
+        const movieLog = get().movieLog;
+        const exist = await watchData.filter((movie) => {
+            return (movie?.name?.toLowerCase() === item?.name.toLowerCase() && movie?.year === item?.year) ||
+                (movie?.url === item?.url) || (movie?.id === item?.watchDate);
+        })?.length >= 1;
+        if (exist) return false;
         set((state) => ({
             ...state,
             movieLog: [item, ...state.movieLog]
         }));
+
         return true;
     },
 }));
